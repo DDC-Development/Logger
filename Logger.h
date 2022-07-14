58bde8b6fbef99caa54e52d2587c97522e0981db
Luke Chemeriov <dgm.bailey@outlook.com>
2022-07-14 15:01:52 +0300

    Updated to show translations

--- Logger.h
+++ Logger.h
@@ -27,6 +27,7 @@ public:
         else 
         {
             std::cout << "\x1B[93m[WARNING]: " << "Error message was attempted to be displayed, but failed due to incorrect type definition." << "\x1B[37m" << std::endl;
+            std::cout << "\x1B[93m[WARNING]: " << "Была предпринята попытка отобразить сообщение об ошибке, но она не удалась из-за неправильного определения типа ошибки." << "\x1B[37m" << std::endl;
         }
     }
     void Warning(const char* message)
@@ -38,6 +39,7 @@ public:
         else 
         {
             std::cout << "\x1B[93m[WARNING]: " << "Error message was attempted to be displayed, but failed due to incorrect type definition." << "\x1B[37m" << std::endl;
+            std::cout << "\x1B[93m[WARNING]: " << "Была предпринята попытка отобразить сообщение об ошибке, но она не удалась из-за неправильного определения типа ошибки." << "\x1B[37m" << std::endl;
         }
     }
     void Info(const char* message)
